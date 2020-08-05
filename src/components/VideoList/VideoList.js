c0429428aa00d9998cce735d60126504f30cb8d0
liam roberts <liamroberts77@googlemail.com>
2020-08-05 17:29:05 +0100

    restructered videoitem and imports

--- src/components/VideoList/VideoList.js
+++ src/components/VideoList/VideoList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 
-import VideoItem from "./VideoItem";
+import VideoItem from "../VideoItem/VideoItem";
 
 export default ({ videos, onVideoSelect }) => {
   const listOfVideos = videos.map(video => (
