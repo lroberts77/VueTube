cf7b340b308c31e682cb8f6b2312986b23ff4bbd
liam roberts <liamroberts77@googlemail.com>
2020-08-07 12:47:02 +0100

    added test to render a div in videolist.spec.js

--- src/components/VideoList/VideoList.spec.js
+++ src/components/VideoList/VideoList.spec.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import VideoList from './VideoList';
+import App from '../App';
 
+const defaultProps = {
+  video: null,
+  onVideoSelect: App.onVideoSelect
+};
 
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<VideoList {...setupProps} />);
+};
 
-describe('VideoList', () => {
-
-
+it("should render a div", () => {
+  const wrapper = setup({ videos: [] });
+  expect(wrapper.find('div').length).toBe(1);
 });
