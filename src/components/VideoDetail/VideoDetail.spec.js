428133908d7ccdfcaa8c186776f24d514bd677b2
liam roberts <liamroberts77@googlemail.com>
2020-08-08 19:01:23 +0100

    added test for message when no video is selected

--- src/components/VideoDetail/VideoDetail.spec.js
+++ src/components/VideoDetail/VideoDetail.spec.js
@@ -3,8 +3,29 @@ import { shallow } from 'enzyme';
 import VideoDetail from './VideoDetail';
 
 describe('VideoDetail', () => {
+
   it('should render a <div />', () => {
     const wrapper = shallow(<VideoDetail />);
     expect(wrapper.find('div').length).toEqual(1);
   });
+
+  const defaultProps = {
+    video: null
+  };
+  
+  const setup = (props = {}) => {
+    const setupProps = { ...defaultProps, ...props };
+    return shallow(<VideoDetail {...setupProps} />);
+  };
+
+  describe("if the user hasn't selected a video", () => {
+
+    it("renders a prompt message when there is no video", () => {
+      const wrapper = setup({ video: null });
+      // const message = findByTestAttr(wrapper, "prompt-message");
+      expect(wrapper.length).toBe(1);
+      expect(wrapper.text()).toEqual("Loading...");
+    });
+  });
+  
 }); 
